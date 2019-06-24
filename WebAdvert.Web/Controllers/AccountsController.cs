e46a0cf0ce773671f7a111886848cd37a6a65d1c
Deepindera Dhanda <Deepindera.Dhanda@avanade.com>
2019-06-25 09:48:29 +1000

    Added MVC for Cognito Sign up.

---
+++ WebAdvert.Web/Controllers/AccountsController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAdvert.Web.Models.Accounts;
+
+namespace WebAdvert.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+        public AccountsController(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager, CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _pool = pool;
+
+        }
+        public async Task<IActionResult> Signup()
+        {
+            var model = new SignUpModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignUpModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+
+                if (user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email alreadt exists");
+
+                }
+                else
+                {
+                    user.Attributes.Add(CognitoAttribute.Name.ToString(), model.Email);
+                    var createdUser = await _userManager.CreateAsync(user, model.Password);
+
+                    if (createdUser.Succeeded)
+                    {
+                        RedirectToAction("Confirm");
+                    }
+                }
+            }
+
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> Confirm()
+        {
+            var model = new ConfirmModel();
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Confirm(ConfirmModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if(user.Status == null)
+                {
+                    ModelState.AddModelError("UserNotExists", "Invalid Confirmation");
+                }
+
+                var result = await _userManager.ConfirmEmailAsync(user,model.Code);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
